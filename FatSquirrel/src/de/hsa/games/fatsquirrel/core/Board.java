8b3de53cab7791f31eb79d5613642cf6172dd5a9
Shiroten <shiroten.xisatu@gmail.com>
2017-04-15 16:30:51 +0200

    Landschaftgenerator erstellt.

--- FatSquirrel/src/de/hsa/games/fatsquirrel/core/Board.java
+++ FatSquirrel/src/de/hsa/games/fatsquirrel/core/Board.java
@@ -9,27 +9,27 @@ public class Board {
     private EntitySet set;
     private BoardConfig config;
 
-    public Board(){
+    public Board() {
         set = new EntitySet();
         config = new BoardConfig(new XY(100, 100), 400);
     }
 
-    public Board(XY size){
+    public Board(XY size) {
         set = new EntitySet();
         config = new BoardConfig(new XY(100, 100), 400);
     }
 
-    public EntitySet getSet(){
+    public EntitySet getSet() {
         return set;
     }
 
-    public BoardConfig getConfig(){
+    public BoardConfig getConfig() {
         return config;
     }
 
-    public FlattenedBoard flatten(){
+    public FlattenedBoard flatten() {
         Entity[][] list = new Entity[config.getSize().getY()][config.getSize().getX()];
-        for(int i = 0; i < set.getNumberOfEntities(); i++){
+        for (int i = 0; i < set.getNumberOfEntities(); i++) {
             Entity dummy = set.getEntity(i);
             list[dummy.getCoordinate().getY()][dummy.getCoordinate().getX()] = dummy;
         }
@@ -39,13 +39,90 @@ public class Board {
         return fb;
     }
 
-    //TODO: Landschaftsgenerator
-    public EntitySet initBoard(XY size){
+    public EntitySet initBoard() {
         EntitySet ret = new EntitySet();
 
+        //Äußere Mauern
+        int WallIDs = setID(EntityType.WALL);
+        int x = config.getSize().getX(), y = config.getSize().getY();
+        for (int i = 0; i < x; i++) {
+            ret.add(new Wall(WallIDs, new XY(i, 0)));
+            ret.add(new Wall(WallIDs, new XY(i, y)));
+        }
+        for (int i = 1; i < y - 1; i++) {
+            ret.add(new Wall(WallIDs, new XY(0, i)));
+            ret.add(new Wall(WallIDs, new XY(x, i)));
+        }
 
+        //Random Entitys auf der Map verteilt
+        randomAddEntity(ret, EntityType.WALL, config.getNumberOfWalls());
+        randomAddEntity(ret, EntityType.BADBEAST, config.getNumberOfBadBeast());
+        randomAddEntity(ret, EntityType.BADPLANT, config.getNumberOfBadPlant());
+        randomAddEntity(ret, EntityType.GOODBEAST, config.getNumberOfGoodBeast());
+        randomAddEntity(ret, EntityType.GOODPLANT, config.getNumberOfGoodPlant());
 
         return ret;
+    }
+
+    public int setID(EntityType type) {
+
+        //todo: IDs in abhängigkeit von irgedtwas vergeben.
+        return -1;
+    }
+
+    public void randomAddEntity(EntitySet set, EntityType type, int ammount) {
+
+        XY position = randomPosition(config.getSize(), set);
+        int randomX = position.getX(), randomY = position.getY();
+
+        Entity addEntity = null;
+        for (int i = 0; i < ammount; i++) {
+            switch (type) {
+                case WALL:
+                    addEntity = new Wall(setID(EntityType.WALL), new XY(randomX, randomY));
+                    break;
+                case BADBEAST:
+                    addEntity = new BadBeast(setID(EntityType.BADBEAST), new XY(randomX, randomY));
+                    break;
+                case BADPLANT:
+                    addEntity = new BadPlant(setID(EntityType.BADPLANT), new XY(randomX, randomY));
+                    break;
+                case GOODBEAST:
+                    addEntity = new GoodBeast(setID(EntityType.GOODBEAST), new XY(randomX, randomY));
+                    break;
+                case GOODPLANT:
+                    addEntity = new GoodPlant(setID(EntityType.GOODPLANT), new XY(randomX, randomY));
+                    break;
+            }
+            set.add(addEntity);
+        }
+
+    }
+
+    public XY randomPosition(XY size, EntitySet set) {
+
+        boolean notDone = true;
+        int newX = 0, newY = 0;
+
+        while (notDone) {
+            boolean noCollision = true;
+            newX = (int) ((Math.random() * size.getX()));
+            newY = (int) ((Math.random() * size.getY()));
+
+            //Durchsuchen des Entityset nach möglichen Konflikten
+            for (int i = 0; i < set.getNumberOfEntities(); i++) {
+                int tempX = set.getEntity(i).getCoordinate().getX();
+                int tempY = set.getEntity(i).getCoordinate().getY();
+
+                if (newX == tempX && newY == tempY) {
+                    noCollision = false;
+                }
+            }
+            if (noCollision) {
+                notDone = false;
+            }
+        }
+        return new XY(newX, newY);
     }
 
     //TODO: toString
--- FatSquirrel/src/de/hsa/games/fatsquirrel/core/BoardConfig.java
+++ FatSquirrel/src/de/hsa/games/fatsquirrel/core/BoardConfig.java
@@ -9,17 +9,56 @@ public class BoardConfig {
     private final XY size;
     private final int wallCount;
 
-    public BoardConfig(XY size, int wallCount){
+    private final int numberOfGoodBeast;
+    private final int numberOfBadBeast;
+    private final int numberOfGoodPlant;
+    private final int numberOfBadPlant;
+    private final int numberOfWalls;
+
+
+    public BoardConfig(XY size, int wallCount, int numberOfGoodBeast, int numberOfBadBeast, int numberOfGoodPlant, int numberOfBadPlant, int numberOfWalls) {
         this.size = size;
         this.wallCount = wallCount;
+        this.numberOfGoodBeast = numberOfGoodBeast;
+        this.numberOfBadBeast = numberOfBadBeast;
+        this.numberOfGoodPlant = numberOfGoodPlant;
+        this.numberOfBadPlant = numberOfBadPlant;
+        this.numberOfWalls = numberOfWalls;
     }
 
-    public XY getSize(){
+    public BoardConfig(XY size, int wallCount) {
+        this(size, wallCount, 7, 7, 7, 7, 7);
+
+    }
+
+
+    public XY getSize() {
         return size;
     }
 
-    public int getWallCount(){
+    public int getWallCount() {
         return wallCount;
+    }
+
+
+    public int getNumberOfGoodBeast() {
+        return numberOfGoodBeast;
+    }
+
+    public int getNumberOfBadBeast() {
+        return numberOfBadBeast;
+    }
+
+    public int getNumberOfGoodPlant() {
+        return numberOfGoodPlant;
+    }
+
+    public int getNumberOfBadPlant() {
+        return numberOfBadPlant;
+    }
+
+    public int getNumberOfWalls() {
+        return numberOfWalls;
     }
 
 }
