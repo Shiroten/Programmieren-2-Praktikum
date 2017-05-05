bb56dec187af121d9af575de9b1056ebe2768949
Tillman Rossa <tillman.rossa@hs-augsburg.de>
2017-05-05 11:38:06 +0200

    TODO hinzugefügt

--- FatSquirrel/src/de/hsa/games/fatsquirrel/util/ui/CommandScanner.java
+++ FatSquirrel/src/de/hsa/games/fatsquirrel/util/ui/CommandScanner.java
@@ -12,39 +12,47 @@ public class CommandScanner {
     private BufferedReader inputReader;
     private PrintStream outputStream;
 
-    public CommandScanner(CommandTypeInfo[] commandTypeInfos, BufferedReader inputReader){
+    public CommandScanner(CommandTypeInfo[] commandTypeInfos, BufferedReader inputReader) {
         this.commandTypeInfos = commandTypeInfos;
         this.inputReader = inputReader;
     }
 
-    public Command next(){
+    public Command next() {
         CommandTypeInfo commandType = null;
         Object[] params = null;
         System.out.println("Geben Sie Ihren Befehl ein: ");
         try {
             String input = inputReader.readLine();
             String[] inputSplit = input.split(" ");
-            for(CommandTypeInfo i : commandTypeInfos){
-                if(i.getName().equals(inputSplit[0])) {
+            for (CommandTypeInfo i : commandTypeInfos) {
+                if (i.getName().equals(inputSplit[0])) {
                     commandType = i;
                     params = new Object[i.getParamTypes().length];
                 }
             }
-            if(commandType == null)
+            //TODO: ScanException
+            if (commandType == null)
                 return new Command(null, null);
-            for(int i = 0; i < params.length; i++){
-                try{
-                    String className = commandType.getParamTypes()[i].getName();
-                    Object object = Class.forName(className);
+            for (int i = 0; i < params.length; i++) {
+                try {
                     //TODO: Object den entsprechenden Teil des String zuweisen
-
-                }catch (ClassNotFoundException e){
+                    switch (commandType.getParamTypes()[i].getCanonicalName()) {
+                        case "int":
+                            params[i] = Integer.parseInt(inputSplit[i]);
+                            break;
+                        case "String":
+                            params[i] = inputSplit[i];
+                            break;
+                        case "float":
+                            params[i] = Float.parseFloat(inputSplit[i]);
+                            break;
+                    }
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
-                params[i] = inputSplit[i];
             }
 
-        } catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
